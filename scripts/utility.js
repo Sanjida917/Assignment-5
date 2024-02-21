c52e59a7152d81a7480e2c4f662a000d3b3bac49
Sanjida Rahman <sanjidamstar17@gmail.com>
2024-02-22 01:29:43 +0600

    updated

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,95 +1,58 @@
+// function setBackgroundColorById(elementID) {
 
-
-
-// function getARendomAlphabet() {
-
-//     //get or create an alphabe array
-//     const alphaString = 'A1 A2 A3 A4 B1 B2 B3 B4 C1 C2 C3 C4 D1 D2 D3 D4 E1 E2 E3 E4 F1 F2 F3 F4 G1 G2 G3 G4 H1 H2 H3 H4 I1 I2 I3 I4 J1 J2 J3 J4 ';
-//     const alphabets = alphaString.split(' ');
-//     console.log(alphabets[39]);
-
-//     for (let i = 0; i <= 39; i++) {
-
-//         const ids = alphabets[i];
-
-//         console.log(getElementById(ids))
-
-
-//     document.getElementById(ids).addEventListener('click', function() {
-
-        
-    
-//     })
-    
-//     }
-
-
-//     }
-
-
-
-const test = document.getElementsByClassName('my-btn-class')
-
-        // console.log(test);
-
-        
-           for(let i=0 ; i<4 ; i++){
-
-           
-        for (const tests of test) {
-
-            // console.log(tests.innerText);
-
-            // let count = 0;
-
-
-
-
-
-            tests.addEventListener(('click'), function(){
-            console.log("Clicked In : ",tests.innerText);
-
-            })
-
-           }
-
-
-           }
-
-            // count+=1;
-           
-        
-
-        // document.getElementsByClassName(tests).addEventListener('click',function() {
-
-        //     console.log(tests.innerText);
-
-        // })
-   
-    
-
-//     // get a random number between 0-39
-
-//     // const randomNumber = Math.random() * 39;
-//     // const index = Math.round(randomNumber);
-
-
-//     // const alphabet = alphabets[index];
-//     // console.log(alphabet);
-
-//     // return alphabet;
-
-
-
+//     const elemnt = document.getElementsById(elementID);
+//     elemnt.classList.add('bg-Orange-500');
 // }
 
-// getARendomAlphabet();
+function getElementsTextById(elementId){
+
+    const element = document.getElementById(elementId);
+    const elementsText = element.innerText;
+    return elementsText;
+
+}
+
+function setBackgroundColorById(idName) {
+
+    const element = document.getElementById(idName);
+    element.classList.add('bg-[#1DD100]','text-white')
+}
+
+function getTextElemenValueById(elementId) {
+    const element = document.getElementById(elementId);
+
+    const elementValueText = element.innerText;
+
+    const value = parseInt(elementValueText);
+
+    return value;
+}
 
 
-// // function getElementValueById(elementId) {
+function setElementById(elementId, value) {
 
-   
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+
+}
+
+function getElementsValueById(elementId){
+
+    const element = document.getElementById(elementId);
+    const elementValue = element.value;
+    return elementValue;
+}
 
 
-// // }
+function hideElementById(elementId) {
 
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+
+function showElementById(elementId) {
+
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
